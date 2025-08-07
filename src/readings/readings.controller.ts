09484ab36156c54b8f797fd44657c22f70b176da
João Winkelmann <hmaster.studio@gmail.com>
2025-08-07 00:03:55 -0300

    chore: Updating API route

--- src/readings/readings.controller.ts
+++ src/readings/readings.controller.ts
@@ -2,12 +2,12 @@ import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { ReadingsService } from './readings.service';
 import { CreateReadingDto } from './dto/create-reading.dto';
 
-@Controller('tempmesh')
+@Controller('readings')
 export class ReadingsController {
   constructor(private readonly readingsService: ReadingsService) {}
 
   // (temporário) POST => /tempmesh/data
-  @Post('data')
+  @Post('add')
   @HttpCode(HttpStatus.CREATED) // HTTP 201 se conseguiu criar
   async createReadings(@Body() createReadingDto: CreateReadingDto) {
     const result = await this.readingsService.createSensorReadings(
