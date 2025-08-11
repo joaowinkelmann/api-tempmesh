08986a8f10c925043993b9e48636cc96788f3999
jwinkelmann <jwinkelmann@imply.com>
2025-08-10 22:50:30 -0300

    chore: Add CORS

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,15 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
+  // Enable CORS for allowed origins
+  app.enableCors({
+    origin: [
+      'http://localhost:5173', // Local frontend
+      'https://winkels.com.br', // Prod frontend
+    ],
+    credentials: true,
+  });
+
   await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
