20952e34fbb03c4c3d16b0418a7e2819a0724c19
João Winkelmann <hmaster.studio@gmail.com>
2025-08-13 00:21:47 -0300

    chore: Adding debug log to login route

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, Logger } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 
@@ -7,9 +7,12 @@ export class AuthService {
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
+    private readonly logger = new Logger(AuthService.name)
   ) {}
 
   async signIn(email: string, password: string) {
+    this.logger.log(`Login attempt from email: ${email}`);
+
     const user = await this.usersService.validatePassword(email, password);
     if (!user) {
       throw new UnauthorizedException('Invalid credentials');
