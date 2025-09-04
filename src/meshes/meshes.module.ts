c1c2e8072e05d1668d6c31bdfc5d0c64c07bcb6b
jwinkelmann <jwinkelmann@imply.com>
2025-09-04 19:15:23 -0300

    fix: Adidng device service to the meshes module

--- src/meshes/meshes.module.ts
+++ src/meshes/meshes.module.ts
@@ -3,9 +3,9 @@ import { MeshesService } from './meshes.service';
 import { MeshesController } from './meshes.controller';
 import { PrismaService } from '../../prisma/prisma.service';
 import { ZonesService } from '../zones/zones.service';
-
+import { DevicesService } from '../devices/devices.service';
 @Module({
   controllers: [MeshesController],
-  providers: [MeshesService, PrismaService, ZonesService],
+  providers: [MeshesService, PrismaService, ZonesService, DevicesService],
 })
 export class MeshesModule {}
